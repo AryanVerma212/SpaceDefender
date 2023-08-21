91e85a86c7afeb1a8c19a12727208e1b5fd98263
AryanVerma212 <aryanverma212@outlook.com>
2023-08-21 19:15:59 +0530

    Added player sprite on the screen

--- main.py
+++ main.py
@@ -5,11 +5,18 @@ pygame.init();
 displayresolutionX,displayresolutionY=1280,720;
 # Create a screen
 screen = pygame.display.set_mode((displayresolutionX,displayresolutionY));
-screen.fill((255,0,0));
+screen.fill((20,20,20)); #dark grey screen in RGB values
 #Icon and Title
 pygame.display.set_caption("SPACE_DEFENDER")
 icon=pygame.image.load('icon.png');
 pygame.display.set_icon(icon);
+
+#Player
+playerImg=pygame.image.load('spaceship.png');
+playerX,playerY=(displayresolutionX/2)*0.95,displayresolutionY*0.9;
+
+def player():
+    screen.blit(playerImg,(playerX,playerY));
 
 #Game Loop
 running=True;
@@ -18,6 +25,7 @@ while running:
         if event.type==pygame.QUIT:
             running=False;
     
+    player();
     pygame.display.update();
 
 
