43e3f020d15a60737a372730b97ca82e7d1fcb89
AryanVerma212 <aryanverma212@outlook.com>
2023-08-21 18:12:28 +0530

    Changed BG colour and added the update function

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
-import pygame as pg;
+import pygame;
 
 # Initialize pygame
-pg.init();
-
+pygame.init();
+displayresolutionX,displayresolutionY=1280,720;
 # Create a screen
-screen = pg.display.set_mode((1280,720));
-
+screen = pygame.display.set_mode((displayresolutionX,displayresolutionY));
+screen.fill((255,0,0));
+#Icon and Title
+pygame.display.set_caption("SPACE_DEFENDER")
+icon=pygame.image.load('icon.png');
+pygame.display.set_icon(icon);
 
 #Game Loop
 running=True;
 while running:
-    for event in pg.event.get():
-        if event.type==pg.QUIT:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
             running=False;
+    
+    pygame.display.update();
+
+
