65593a72d36a4c00f43035bfcbfb40cc1bee5178
AryanVerma212 <aryanverma212@outlook.com>
2023-08-22 15:46:15 +0530

    Added enemy movements and non-lethal bullet mechanics
    The bullet currently does not kill the enemy but just passes through the sprite

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import pygame;
+import pygame; 
+import random; 
 
 # Initialize pygame
 pygame.init(); 
 displayresolutionX,displayresolutionY=1280,720; 
+
 # Create a screen
 screen = pygame.display.set_mode((displayresolutionX,displayresolutionY)); 
 
@@ -11,42 +13,72 @@ pygame.display.set_caption("SPACE_DEFENDER")
 icon=pygame.image.load('icon.png'); 
 pygame.display.set_icon(icon); 
 
+#Background
+background=pygame.image.load('bg.jpg'); 
+
+#Enemy
+enemyImg=pygame.image.load('enemy.png'); 
+enemyX,enemyY= random.randint(0,displayresolutionX-64),0; 
+enemySpeed=0.9; 
+enemyX_change=enemySpeed; 
+enemyY_change=32; 
+
+
 #Player
 playerImg=pygame.image.load('spaceship.png'); 
-playerX,playerY=(displayresolutionX/2)*0.95,displayresolutionY*0.9; 
+playerX,playerY= displayresolutionX/2-32,displayresolutionY-64; 
 playerX_change=0; 
 playerY_change=0; 
-speed=0.5; 
+playerSpeed=0.6; 
+
+#Bullet
+#Ready State:
+bulletImg=pygame.image.load('bullet.png'); 
+bulletX,bulletY=playerX,playerY; 
+bulletY_change=2; 
+bulletState='ready'; 
+
+def fireBullet(X,Y):
+    global bulletState; 
+    bulletState='fire'; 
+    screen.blit(bulletImg,(X+16,Y+10)); 
 
 def player(X,Y):
     screen.blit(playerImg,(X,Y)); 
+
+def enemy(X,Y):
+    screen.blit(enemyImg,(X,Y)); 
 
 #Game Loop
 running=True; 
 while running:
     screen.fill((20,20,20)); #dark grey screen in RGB values
+    screen.blit(background,(0,0)); 
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             running=False; 
 
         if event.type==pygame.KEYDOWN:
             if event.key==pygame.K_LEFT:
-                playerX_change=-speed; 
+                playerX_change=-playerSpeed; 
             if event.key==pygame.K_RIGHT:
-                playerX_change=speed; 
+                playerX_change=playerSpeed; 
             
             if event.key==pygame.K_UP:
-                playerY_change=-speed; 
+                playerY_change=-playerSpeed; 
             if event.key==pygame.K_DOWN:
-                playerY_change=speed; 
+                playerY_change=playerSpeed; 
+        
+            if event.key==pygame.K_SPACE:
+                fireBullet(playerX,playerY); 
             
 
         if event.type==pygame.KEYUP:
             if event.key==pygame.K_LEFT or event.key==pygame.K_RIGHT:
                 playerX_change=0; 
-        if event.type==pygame.KEYUP:
             if event.key==pygame.K_UP or event.key==pygame.K_DOWN:
                 playerY_change=0; 
+
     
     playerX+=playerX_change; 
     if playerX>displayresolutionX-64: #64 is the size of image
@@ -60,8 +92,26 @@ while running:
     if playerY<0:
         playerY=0; 
     
+    enemyX+=enemyX_change; 
+    if enemyX>displayresolutionX-64: #64 is the size of image
+        enemyX_change=-enemySpeed;  
+        enemyY+=enemyY_change; 
+    if enemyX<0:
+        enemyX_change=enemySpeed; 
+        enemyY+=enemyY_change; 
+    
+    #Bullet Movement
+    if bulletY<-32: 
+        bulletState='ready'; 
+    if bulletState=='ready':
+        bulletX,bulletY=playerX,playerY; 
+    if bulletState=='fire':
+        fireBullet(bulletX,bulletY); 
+        bulletY-=bulletY_change; 
+    
 
     player(playerX,playerY); 
+    enemy(enemyX,enemyY); 
     pygame.display.update(); 
 
 
